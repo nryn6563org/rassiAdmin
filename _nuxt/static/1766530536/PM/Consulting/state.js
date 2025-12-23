46f633474905e10d197ce431d75ee98c25c0b786
jhLee <77824683+nryn6563org@users.noreply.github.com>
2025-12-24 07:55:38 +0900

    Updates

---
+++ _nuxt/static/1766530536/PM/Consulting/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FrassiAdmin\u002F_nuxt\u002Fstatic\u002F1766530536",layout:"default",error:a,state:{pageTitle:""},serverRendered:true,routePath:"\u002FPM\u002FConsulting",config:{_app:{basePath:"\u002FrassiAdmin\u002F",assetsPath:"\u002FrassiAdmin\u002F_nuxt\u002F",cdnURL:a}}}}(null));
