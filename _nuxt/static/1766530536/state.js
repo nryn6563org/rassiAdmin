56b4ccc31a14e150d28a78db31bf5e9bcfdee7a4
jhLee <77824683+nryn6563org@users.noreply.github.com>
2025-12-24 08:14:19 +0900

    Updates

--- _nuxt/static/1766530536/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FrassiAdmin\u002F_nuxt\u002Fstatic\u002F1766530536",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002F",config:{_app:{basePath:"\u002FrassiAdmin\u002F",assetsPath:"\u002FrassiAdmin\u002F_nuxt\u002F",cdnURL:a}}}}(null));
