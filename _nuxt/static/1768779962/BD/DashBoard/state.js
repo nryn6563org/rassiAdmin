64dbba740c08a7010c6c13c6a7e93d43a9c3a8e6
jhLee <77824683+nryn6563org@users.noreply.github.com>
2026-02-23 08:28:11 +0900

    Updates

--- _nuxt/static/1768779962/BD/DashBoard/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FrassiAdmin\u002F_nuxt\u002Fstatic\u002F1768779962",layout:"default",error:a,state:{pageTitle:""},serverRendered:true,routePath:"\u002FBD\u002FDashBoard",config:{_app:{basePath:"\u002FrassiAdmin\u002F",assetsPath:"\u002FrassiAdmin\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ _nuxt/static/1771802889/IR/Signal/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FrassiAdmin\u002F_nuxt\u002Fstatic\u002F1771802889",layout:"default",error:a,state:{pageTitle:""},serverRendered:true,routePath:"\u002FIR\u002FSignal",config:{_app:{basePath:"\u002FrassiAdmin\u002F",assetsPath:"\u002FrassiAdmin\u002F_nuxt\u002F",cdnURL:a}}}}(null));
